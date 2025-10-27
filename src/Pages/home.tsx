2c59fe9d9eebdc7e35672ca3b57a87026fea1b49
Ben Siri <ben@acsiri.com>
2025-10-27 12:25:56 -0400

    First commit

---
+++ src/Pages/home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Data Strucures and Algorithms Practice in React!</h1>
+      <ol>
+        <li>
+          <a href="/isUnique">Problem 1: Is Unique</a>
+        </li>
+      </ol>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Pages/isUnique.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Problem1: React.FC = () => {
+  return (
+    <div>
+      <h1>Problem 1 Page</h1>
+      <p>This is the content for Problem 1.</p>
+    </div>
+  );
+};
+
+export default Problem1;
